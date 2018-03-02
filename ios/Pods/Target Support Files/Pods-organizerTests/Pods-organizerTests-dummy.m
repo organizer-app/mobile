67982df45c0423c82a8484188bd4726ae83e1e13
Jeffrey Goldsmith <phoenixofstone@yahoo.ca>
2018-03-02 02:53:15 -0500

    Added google sso

---
+++ ios/Pods/Target Support Files/Pods-organizerTests/Pods-organizerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_organizerTests : NSObject
+@end
+@implementation PodsDummy_Pods_organizerTests
+@end
--- src/loginPage/loginPage.component.js
+++ src/loginPage/loginPage.component.js
@@ -1,28 +1,88 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {Actions} from 'react-native-router-flux';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 
-class LoginPage extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Button
-          onPress={Actions.home}
-          title="Login"
-        />
-      </View>
-    );
-  }
-}
-
-var styles = StyleSheet.create({
-  containter: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: '#F5FCFF',
+ },
+ welcome: {
+   fontSize: 20,
+   textAlign: 'center',
+   margin: 10,
+ },
+ instructions: {
+   textAlign: 'center',
+   color: '#333333',
+   marginBottom: 5,
+ },
 });
+class LoginPage extends Component
+{
+ constructor(props)
+ {
+   super(props);
+   this.state = { user: null };
+ }
+
+ componentDidMount()
+ {
+   this._setupGoogleSignin();
+ }
+
+ _setupGoogleSignin()
+ {
+   try {
+    GoogleSignin.configure({
+      iosClientId: '153497318804-fv3cc14468utuqri4g6upju4mvodr0a0.apps.googleusercontent.com',
+      offlineAccess: false
+    });
+   }
+   catch(err) {
+     console.log("Google signin error", err.code, err.message);
+   }
+ }
+
+ _signIn()
+ {
+   GoogleSignin.signIn()
+   .then((user) => {
+     console.log(user);
+     this.setState({user: user});
+   })
+   .catch((err) => {
+     console.log('WRONG SIGNIN', err);
+   })
+   .done();
+ }
+
+ render()
+ {
+   const { user } = this.state;
+
+   if (!user)
+   {
+     return (
+       <View style={styles.container}>
+         <GoogleSigninButton
+           style={{ width: 312, height: 48 }}
+           size={GoogleSigninButton.Size.Wide}
+           color={GoogleSigninButton.Color.Dark}
+           onPress={this._signIn.bind(this)}
+         />
+       </View>
+     );
+   } else {
+     return (
+       <View style={styles.container}>
+         <Text>You're logged in!</Text>
+       </View>
+     );
+   }
+ }
+}
 
 export default LoginPage;
